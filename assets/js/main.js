70884910fb17c7cf81482550cf61dca54b759e25
kwuraa <kwuraa@gmail.com>
2025-01-23 17:22:19 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,59 @@
+function updateProfileInfo(profileData) {
+  const photo = document.getElementById("profile.photo");
+  photo.src = profileData.photo;
+  photo.alt = profileData.name;
+
+  const name = document.getElementById("profile.name");
+  name.textContent = profileData.name;
+
+  const job = document.getElementById("profile.job");
+  job.textContent = profileData.job;
+
+  const location = document.getElementById("profile.location");
+  location.textContent = profileData.location;
+
+  const phone = document.getElementById("profile.phone");
+  phone.textContent = profileData.phone;
+  phone.href = `tell:${profileData.phone}`;
+
+  const email = document.getElementById("profile.email");
+  email.textContent = profileData.email;
+  email.href = `mailto:${profileData.email}`;
+}
+
+function updateSoftSkills(profileData) {
+  const softSkills = document.getElementById("profile.skills.softSkills");
+  softSkills.innerHTML = profileData.skills.softSkills
+    .map((skill) => `<li>${skill}</li>`)
+    .join("");
+}
+
+function updateHardSkills(profileData) {
+  const hardSkills = document.getElementById("profile.skills.hardSkills");
+  hardSkills.innerHTML = profileData.skills.hardSkills
+    .map(
+      (skill) => `<li>
+    <img
+      src="${skill.logo}"
+      alt="${skill.name}"
+      title="${skill.name}"
+    />
+  </li>`
+    )
+    .join("");
+}
+
+function updateLang(profileData) {
+  const languages = document.getElementById("languages");
+  languages.innerHTML = profileData.languages
+    .map((lang) => `<li>${lang}</li>`)
+    .join("");
+}
+
+(async () => {
+  const profileData = await fetchProfileData();
+  updateProfileInfo(profileData);
+  updateSoftSkills(profileData);
+  updateHardSkills(profileData);
+  updateLang(profileData);
+})();
